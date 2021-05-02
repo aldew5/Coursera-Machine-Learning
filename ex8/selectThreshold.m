e261d6df4c3769bfc9b79c2ea5848fd124ded8be
Alec Dewulf <alec@cisudbury.ca>
2021-05-02 12:47:47 -0400

    finished first part of ex8

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -23,17 +23,17 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    predictions = (pval < epsilon);
+    
+    % calculate values for f1
+    fn = sum((predictions == 0) & (yval == 1));
+    tp = sum((predictions == 1) & (yval == 1));
+    fp = sum((predictions == 1) & (yval == 0));
+    
+    precision = tp / (tp + fp);
+    recall = tp / (tp + fn);
 
-
-
-
-
-
-
-
-
-
-
+    F1 = 2 * precision * recall / (precision + recall);
 
     % =============================================================
 
